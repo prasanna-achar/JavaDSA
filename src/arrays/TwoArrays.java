673772359e7c7ae536495269ea20061e1df94dec
Prasanna Achar <acharprasanna5@gmail.com>
2025-12-16 14:08:02 +0530

    small change but still BigO(m+n) :Problem ::Find Median of two array in bigO(m + n) complexity

--- src/arrays/TwoArrays.java
+++ src/arrays/TwoArrays.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 public class TwoArrays {
 	
 	private static int[] mergeSortedArr(int[] ar1, int[] ar2) {
-		int[] mergedArr = new int[ar1.length + ar2.length];
+		int m = ((ar1.length + ar2.length) / 2) + 1;
+		int[] mergedArr = new int[m];
 		int i= 0, j =0 , k = 0;
-		while(i < ar1.length && j < ar2.length) {
+		while(i < ar1.length && j < ar2.length && k < m) {
 			if(ar1[i] < ar2[j]) {
 				mergedArr[k++] = ar1[i++];
 			}else {
@@ -16,10 +17,10 @@ public class TwoArrays {
 			}
 		}
 		
-		while(i < ar1.length) {
+		while(i < ar1.length && k < m) {
 			mergedArr[k++] = ar1[i++];			
 		}
-		while(j < ar2.length) {
+		while(j < ar2.length && k < m) {
 			mergedArr[k++] = ar2[j++];			
 		}
 		return mergedArr;
@@ -28,11 +29,10 @@ public class TwoArrays {
 	public static int findMedian(int[] ar1, int[] ar2) {
 		int[] mergedArr = mergeSortedArr(ar1, ar2);
 		System.out.println(Arrays.toString(mergedArr));
-		int m = mergedArr.length / 2;
-		if(mergedArr.length % 2 != 0) {
-			return mergedArr[m];
+		if((ar1.length + ar2.length) % 2 != 0 ) {
+			return mergedArr[mergedArr.length-1];
 		}else {
-			return (mergedArr[m] + mergedArr[m-1]) / 2;
+			return (mergedArr[mergedArr.length-1] + mergedArr[mergedArr.length-2]) / 2;
 		}
 	}
 }
